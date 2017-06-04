216b3d54e543f4171e48f097f2559646872b5f7f
Nitish Chauhan <chauhannitish1995@gmail.com>
2017-06-04 11:40:19 +0530

    spy chat after updating with dictinory

--- try.py
+++ try.py
@@ -1,12 +1,20 @@
-n = 100
-m = 200
+loop_count = 0
+while (True):
+    if loop_count > 4:
+        print "You are too DUMB to be a SPY or your keyboard need to be repaired, Bye bye."
+        exit()
 
+    loop_count = loop_count + 1
+    spy_rating = raw_input("What is your spy rating? ")
+    spy_rating = spy_rating.replace(" ", "")
+    try:
+        spy_rating = float(spy_rating)
+        if spy_rating < 10.0:
+            break
+        else:
+            print "We are expecting rating out of 10"
+    except:
+        print "Please enter a valid rating."
 
-def add():
-    m = m + n
-    return m
-
-
-
-k = add()
-print k
+print spy_rating
+print type(spy_rating)
